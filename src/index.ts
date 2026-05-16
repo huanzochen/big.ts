5fea6ee7007936f5b00c61db8c1f7edde48c64c1
ziquanh19 <cy94295@gmail.com>
2026-05-16 18:31:01 +0800

    add test

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
-console.log("Hello! Dev mode is now watching for changes...");
-console.log("Try modifying this file to see the terminal update automatically.");
+import "./demo_usage";
+
+console.log("\n[System] Demo executed successfully.");
